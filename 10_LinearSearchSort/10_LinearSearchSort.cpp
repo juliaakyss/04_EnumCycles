fa4ccc7812da15a68da85044279242a883130344
juliaakyss <juliaakyss@gmail.com>
2026-09-21 16:47:25 +0300

    linear search sort

--- 10_LinearSearchSort/10_LinearSearchSort.cpp
+++ 10_LinearSearchSort/10_LinearSearchSort.cpp
@@ -61,3 +61,53 @@ void ShowArray(int arr[], int size) {
 	} 
 	cout << endl; 
 }
+void SortBetweenNegatives(int arr[], int size) {
+    int firstNeg = -1;
+    int lastNeg = -1;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            firstNeg = i;
+            break;
+        }
+    }
+    for (int i = size - 1; i >= 0; i--) {
+        if (arr[i] < 0) {
+            lastNeg = i;
+            break;
+        }
+    }
+    if (firstNeg == -1) {
+        cout << "There is no negative elements in array" << endl;
+        return;
+    }
+    if (firstNeg == lastNeg) {
+        cout << "There is onle one negative element (index " << firstNeg << ")" << endl;
+        return;
+    }
+    if (lastNeg - firstNeg <= 1) {
+        cout << "There are no other numbers between the first and last negative elements." << endl;
+        return;
+    }
+    cout << "The leftmost negative one [index " << firstNeg << "]: " << arr[firstNeg] << endl;
+    cout << "The rightmost negative one [index " << lastNeg << "]: " << arr[lastNeg] << endl;
+    for (int i = firstNeg + 1; i < lastNeg; i++) {
+        int temp = arr[i];
+        int j;
+        for (j = i - 1; j > firstNeg && arr[j] > temp; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = temp;
+    }
+}
+int main() {
+    srand(time(0));
+    const int size = 10;
+    int arr[size];
+    InitArray(arr, size);
+    cout << "Array before changes:" << endl;
+    ShowArray(arr, size);
+    cout << endl;
+    SortBetweenNegatives(arr, size);
+    cout << "\nArray after sorting:" << endl;
+    ShowArray(arr, size);
+}
